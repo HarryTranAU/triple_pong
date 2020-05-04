e58ed6c92984b0530fa5bffc1a5cdb527943fe8d
HarryTranAU <tranharry@live.com.au>
2020-05-04 17:09:18 +1000

    implement ball movement, ball reset

--- pong.py
+++ pong.py
@@ -34,6 +34,9 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+# Ball speed
+ball.dx = 0.2
+ball.dy = -0.2
 
 # Function
 def paddle_a_up():
@@ -61,8 +64,29 @@ wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down") 
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
     wn.update()
+
+    # Move the Ball
+    ball.setx(ball.xcor()+ball.dx)
+    ball.sety(ball.ycor()+ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
